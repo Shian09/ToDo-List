b2e631668271749f0ee7b496d0ef019c07db2197
Shian09 <shiankhan@iut-dhaka.edu>
2020-05-19 22:56:26 +0600

    A simple todo list to manage
    A list of daily work that can be managed. It has been developed using React.

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+ function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>
+                This is a TODO list app. The version is 1.0.0.
+            </p>
+        </React.Fragment>
+    )
+}
+
+
+export default About
